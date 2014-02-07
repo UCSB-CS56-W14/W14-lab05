a0b516670ca51d3a9e5e9a084c039a5554230d5c
Ziping He <ziping_he@umail.ucsb.edu>
2014-02-06 17:37:18 -0800

    copied and tested two files from lab04

---
+++ src/edu/ucsb/cs56/w14/lab05/hzpkk520/Usps.java
@@ -0,0 +1,83 @@
+
+package edu.ucsb.cs56.w14.lab05.hzpkk520;
+
+/** class Usps
+
+    @author Phill Conrad
+    @author Yue Xin
+    @author Ziping He
+    @version 2014/02/06 for lab04, cs56, W14
+    @see Usps
+
+*/
+
+public class Usps {
+    private String trackingNum; // tracking number, e.g "9405509" 
+    // note: String because leading zeros may be significant, or value may exceed range of an int
+
+    private String name; // name of the receiver, e.g "Ariel"
+    
+    /**
+       Two-arg constructor
+       @param trackingNum tracking number, e.g	"9405509"
+       @param name name of the receiver, e.g "Ariel"
+    */
+
+    public Usps(String trackingNum, String name) {
+      this.trackingNum=trackingNum;
+      this.name=name;
+    }
+
+    /** 
+	Getter for trackingNum
+	@return tracking number, e.g "9405509"
+    */
+
+    public String getTrackingNum () { return this.trackingNum; }
+
+    /** 
+	Getter for name
+	@return name of the receiver, e.g "Ariel"
+    */
+    public String getName () {return this.name; }
+
+    /**
+       convert to String representation in format [trackingNum,name]
+       e.g. <code>["9405509","Ariel"]</code>
+
+       @return formatted string, [trackingNum,name]
+    */
+
+    @Override
+    public String toString() {
+	return "[" + this.trackingNum + "," + this.name + "]";
+    }
+
+
+    /** hashcode to be used for hash tables, etc.
+	based only on the trackingNum value.
+     */
+
+    @Override
+    public int hashCode() {
+	final int prime=31;
+	int result=1;
+	result=prime * result + trackingNum.hashCode();
+	return result;
+    }
+
+    /** compare for equality based on trackingNum
+	@return true if parameter is a Usps with same trackingNum, otherwise false
+     */
+
+    @Override
+    public boolean equals(Object o) {
+       if (o==null) {return false; }
+       if (! (o instanceof Usps) ) {return false;}
+       Usps p = (Usps) o;
+       if(this.trackingNum==p.trackingNum)
+	return true;
+       else
+        return false;
+    }
+}
---
+++ src/edu/ucsb/cs56/w14/lab05/hzpkk520/UspsTest.java
@@ -0,0 +1,99 @@
+package edu.ucsb.cs56.w14.lab05.hzpkk520;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+
+// @@@ TODO: Change the items below per instructions.
+// @@@ TODO: Don't forget to change the "see" comment to refer to your class
+// @@@ TODO: Then delete these @@@ TODO lines.
+
+/** Test class for Usps
+
+    @author Phill Conrad
+    @author Yue Xin
+    @author Ziping He
+    @version 2014/02/06 for lab04, cs56, W14
+    @see Usps 
+
+*/
+
+public class UspsTest {
+
+    /** Test case for getTrackingNum method of Usps
+     @see Usps#getTrackingNum
+    */
+
+    @Test
+	public void test_getTrackingNum() {
+	Usps c1 = new Usps("9405509","Ariel");
+	assertEquals("9405509",c1.getTrackingNum());
+    }
+
+
+    /** Test case for getName method of Usps
+     @see Usps#getName
+    */
+
+    @Test
+	public void test_getName() {
+	Usps c1 = new Usps("9405509","Ariel");
+	assertEquals("Ariel",c1.getName());
+    }
+
+    /** Test case for toString
+     @see Usps#toString
+    */
+
+    @Test
+	public void test_toString() {
+	Usps c1 = new Usps("9405509","Ariel");
+	assertEquals("[9405509,Ariel]",c1.toString());
+    }
+
+    /** Test case 1 for .equals
+     @see Usps#toString
+    */
+
+    @Test
+	public void test_equals1() {
+	Usps c1 = new Usps("9405509","Ariel");
+	Usps c2 = new Usps("9405509","Ariel");
+	assertTrue(c1.equals(c2));
+    }
+
+    /** Test case 2 for .equals
+     @see Usps#toString
+    */
+
+    @Test
+	public void test_equals2() {
+	Usps c1 = new Usps("9405509","Ariel");
+	Usps c2 = new Usps("6999381","Ariel");
+	assertFalse(c1.equals(c2));
+    }
+
+    /** Test case 1 for .hashCode
+     @see Usps#hashCode
+    */
+
+    @Test
+	public void test_hashCode1() {
+	Usps c1 = new Usps("9405509","Ariel");
+	Usps c2 = new Usps("9405509","Ariel");
+	assertTrue(c1.hashCode()==c2.hashCode());
+    }
+
+    /** Test case 2 for .hashCode
+     @see Usps#hashCode
+    */
+
+    @Test
+	public void test_hashCode2() {
+	Usps c1 = new Usps("9405509","Ariel");
+	Usps c3 = new Usps("6999381","Ariel");
+	assertTrue(c1.hashCode()!=c3.hashCode());
+    }
+
+}
