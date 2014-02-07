a022b5324edd5c73e973234ec90f8c8298826ce2
Anthony Hoang <ant.hoang93@gmail.com>
2014-02-06 17:28:12 -0800

    added the two files from lab04

---
+++ src/edu/ucsb/cs56/w14/lab05/ahoang512/Hockey.java
@@ -0,0 +1,76 @@
+package edu. ucsb. cs56. w14. lab05. ahoang512;
+
+public class Hockey {
+    private String ZipCode; // Home zipcode where team plays.
+
+    private String Mascot; // mascot for the team
+    
+    /**
+       Two-arg constructor
+       @param Zip  ZipCode, e.g. 00127288
+       @param masc   Hockey team mascot, e.g "Trader Joe's Raisin Bran"
+    */
+
+    public Hockey(String Zip, String masc) {
+      ZipCode = Zip;
+      Mascot = masc;
+    }
+
+    /** 
+	Getter for ZipCode
+	@return ZipCode, numbers under the bar code, e.g. 00127288
+    */
+
+    public String getZipCode () { return ZipCode; }
+
+    /** 
+	Getter for Mascot
+	@return Mascot, e.g "Sharks"
+    */
+    public String getMascot () {return Mascot; }
+
+    /**
+       convert to String representation in format [zipcode,mascot]
+       e.g. ["00127288","Trader Joe's Raisin Bran"]
+
+       @return formatted string, [zipcode,mascot]
+    */
+
+    @Override
+    public String toString() {
+	return "[" + ZipCode + "," + Mascot + "]";    
+    }
+
+
+    /** hashcode to be used for hash tables, etc.
+	based only on the zipcode value.
+     */
+
+    @Override
+    public int hashCode() {
+        return ZipCode.hashCode(); 
+    }
+
+    /** compare for equality based on zipcode     
+    
+	@return true if parameter is a Hockey with same zipcode, otherwise false
+     */
+
+    @Override
+    public boolean equals(Object o) {
+	if (o == null){
+	    return false;
+	}
+	if (!(o instanceof Hockey)){
+	    return false;
+	}
+	Hockey h = (Hockey)o;
+	return (h.getZipCode() == this.getZipCode());
+ // as if not overridden
+	// OR: return false;
+	// OR: return true;
+	// Note: there is not good "always wrong" stub value for a boolean method
+	//   It will be "correct" half the time by accident.
+    }
+
+}
---
+++ src/edu/ucsb/cs56/w14/lab05/ahoang512/HockeyTest.java
@@ -0,0 +1,94 @@
+package edu. ucsb. cs56. w14. lab05. ahoang512;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+
+/** Test class for Hockey
+
+    @author Phill Conrad
+    @author Anthony Hoang
+    @version 2014/01/30 for lab04, cs56, W14
+    @see Hockey
+
+*/
+
+public class HockeyTest {
+
+    /** Test case for getZipCode method of Hockey
+     @see Hockey#getZipCode
+    */
+
+    @Test
+	public void test_getZipCode() {
+	Hockey c1 = new Hockey("00127288","Trader Joe's Raisin Bran");
+	assertEquals("00127288",c1.getZipCode());
+    }
+
+
+    /** Test case for getMascot method of Hockey
+     @see Hockey#getMascot
+    */
+
+    @Test
+	public void test_getMascot() {
+	Hockey c1 = new Hockey("00127288","Trader Joe's Raisin Bran");
+	assertEquals("Trader Joe's Raisin Bran",c1.getMascot());
+    }
+
+    /** Test case for toString
+     @see Hockey#toString
+    */
+
+    @Test
+	public void test_toString() {
+	Hockey c1 = new Hockey("00127288","Trader Joe's Raisin Bran");
+	assertEquals("[00127288,Trader Joe's Raisin Bran]",c1.toString());
+    }
+
+    /** Test case 1 for .equals
+     @see Hockey#toString
+    */
+
+    @Test
+	public void test_equals1() {
+	Hockey c1 = new Hockey("00127288","Trader Joe's Raisin Bran");
+	Hockey c2 = new Hockey("00127288","Trader Joe's Raisin Bran");
+	assertTrue(c1.equals(c2));
+    }
+
+    /** Test case 2 for .equals
+     @see Hockey#toString
+    */
+
+    @Test
+	public void test_equals2() {
+	Hockey c1 = new Hockey("00127288","Trader Joe's Raisin Bran");
+	Hockey c2 = new Hockey("99999999","Trader Joe's Raisin Bran");
+	assertFalse(c1.equals(c2));
+    }
+
+    /** Test case 1 for .hashCode
+     @see Hockey#hashCode
+    */
+
+    @Test
+	public void test_hashCode1() {
+	Hockey c1 = new Hockey("00127288","Trader Joe's Raisin Bran");
+	Hockey c2 = new Hockey("00127288","Trader Joe's Raisin Bran");
+	assertTrue(c1.hashCode()==c2.hashCode());
+    }
+
+    /** Test case 2 for .hashCode
+     @see Hockey#hashCode
+    */
+
+    @Test
+	public void test_hashCode2() {
+	Hockey c1 = new Hockey("00127288","Trader Joe's Raisin Bran");
+	Hockey c3 = new Hockey("99999999","Trader Joe's Raisin Bran");
+	assertTrue(c1.hashCode()!=c3.hashCode());
+    }
+
+}
