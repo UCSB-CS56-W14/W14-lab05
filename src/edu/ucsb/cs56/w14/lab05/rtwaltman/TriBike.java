72ec422a7f9ebec50ce0ce971a8df445584157f0
Richard Waltman <rtwaltman@umail.ucsb.edu>
2014-02-10 17:27:32 -0800

    Added Lab04 files and tested ant

---
+++ src/edu/ucsb/cs56/w14/lab05/rtwaltman/TriBike.java
@@ -0,0 +1,85 @@
+package edu.ucsb.cs56.w14.lab05.rtwaltman;
+/**
+   <p> TriBike represents a triathlon bike, including the model and registered number.
+
+   @author P.Conrad
+   @author Richard Waltman
+   @version UCSB, CS56, W14
+*/
+
+public class TriBike {
+    private String model; // Make and model of the bike
+
+    private int regNumber; // Registration number of the bike
+    
+    /**
+       Two-arg constructor
+       @param model model of the bike, e.g "Specialized SHIV"
+       @param regNumber regNumber of the bike, e.g "54321"
+    */
+
+    public TriBike(String model, int regNumber) {
+      this.model = model;
+	  this.regNumber = regNumber;
+    }
+
+    /** 
+	Getter for model
+	@return model -or- make and model of the Tri Bike, e.g "Specialized SHIV"
+    */
+    public String getModel () { return this.model; }
+
+
+    /** 
+	Getter for regNumber
+	@return regNumber of the bike, e.g "54321"
+    */
+    public int getRegNumber () {return this.regNumber; }
+
+
+    /**
+       Return for model of bike and registration number
+       @return formatted string, [model, regNumber]
+    */
+    @Override
+    public String toString() {
+	return "[" + this.model + "," + this.regNumber + "]"; // in stub, return super class (as if not overridden)
+	// OR: return "[STUB]";     
+    }
+
+
+    /** Hashcode to be used for hash tables, etc.
+		@return hashed registered number - based ONLY on the registered number value.
+     */
+
+    @Override
+    public int hashCode() {
+        return this.regNumber; //Could be modded with any table size later if needed 
+	// in stub, return super class (as if not overridden)
+	// OR: return 42; // WORST POSSIBLE HASH CODE IN PRACTICE; ok for testing tests
+    }
+
+    /** compare for equality based on model
+	@return true if parameter is a TriBike with same registered number, otherwise false
+     */
+
+    @Override
+    public boolean equals(Object o) {
+    	if(o == null)
+			return false;
+		if(!(o instanceof TriBike))
+			return false;
+
+		TriBike p = (TriBike) o;
+		if(this.regNumber == p.regNumber)
+			return true;
+		else
+			return false;
+	// as if not overridden
+	// OR: return false;
+	// OR: return true;
+	// Note: there is not good "always wrong" stub value for a boolean method
+	//   It will be "correct" half the time by accident.
+    }
+
+}
---
+++ src/edu/ucsb/cs56/w14/lab05/rtwaltman/TriBikeTest.java
@@ -0,0 +1,98 @@
+package edu.ucsb.cs56.w14.lab05.rtwaltman;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+
+// @@@ TODO: Change the items below per instructions.
+// @@@ TODO: Don't forget to change the "see" comment to refer to your class
+// @@@ TODO: Then delete these @@@ TODO lines.
+
+/** Test class for TriBike
+
+    @author Phill Conrad
+    @author Insert your name here
+    @author If working in pair, insert 2nd pair here, else delete this line
+    @version 2014/01/30 for lab04, cs56, W14
+    @see TriBike 
+
+*/
+
+public class TriBikeTest {
+
+    /** Test case for getRegNumber method of TriBike
+     @see TriBike#getRegNumber
+    */
+
+    @Test
+	public void test_getRegNumber() {
+	TriBike c1 = new TriBike("Specialized SHIV",54321);
+	assertEquals(54321,c1.getRegNumber());
+    }
+
+
+    /** Test case for getModel method of TriBike
+     @see TriBike#getModel
+    */
+
+    @Test
+	public void test_getModel() {
+	TriBike c1 = new TriBike("Specialized SHIV",54321);
+	assertEquals("Specialized SHIV",c1.getModel());
+    }
+
+    /** Test case for toString
+     @see TriBike#toString
+    */
+
+    @Test
+	public void test_toString() {
+	TriBike c1 = new TriBike("Specialized SHIV",54321);
+	assertEquals("[Specialized SHIV,54321]",c1.toString());
+    }
+
+    /** Test case 1 for .equals
+     @see TriBike#toString
+    */
+
+    @Test
+	public void test_equals1() {
+	TriBike c1 = new TriBike("Specialized SHIV",54321);
+	TriBike c2 = new TriBike("Specialized SHIV",54321);
+	assertTrue(c1.equals(c2));
+    }
+
+    /** Test case 2 for .equals
+     @see TriBike#toString
+    */
+
+    @Test
+	public void test_equals2() {
+	TriBike c1 = new TriBike("Specialized SHIV",54321);
+	TriBike c2 = new TriBike("99999999",99999);
+	assertFalse(c1.equals(c2));
+    }
+
+    /** Test case 1 for .hashCode
+     @see TriBike#hashCode
+    */
+
+    @Test
+	public void test_hashCode1() {
+	TriBike c1 = new TriBike("Specialized SHIV",54321);
+	TriBike c2 = new TriBike("Specialized SHIV",54321);
+	assertTrue(c1.hashCode()==c2.hashCode());
+    }
+
+    /** Test case 2 for .hashCode
+     @see TriBike#hashCode
+    */
+
+    @Test
+	public void test_hashCode2() {
+	TriBike c1 = new TriBike("Specialized SHIV",54321);
+	TriBike c3 = new TriBike("99999999",99999);
+	assertTrue(c1.hashCode()!=c3.hashCode());
+    }
+
+}
