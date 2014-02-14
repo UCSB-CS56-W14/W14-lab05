cceaf2fed2322fc0320bb0d51e860d249b4c3025
Logan Ortega <loganportega@gmail.com>
2014-02-13 19:10:05 -0800

    added ProteinPowder.java and ProteinPowder.java to new directory

---
+++ src/edu/ucsb/cs56/w14/lab05/loganortega/ProteinPowder.java
@@ -0,0 +1,81 @@
+public class ProteinPowder {
+    private String flavor; // describes flavor of powder, e.g. "chocolate"
+
+    private int amountProtein; // amount of protein per serving
+
+    private int serialNumber; // directly identifies brand and flavor of protein, 8-digit number
+    
+    /**
+       Three-arg constructor
+       @param serialNumber numbers by the bar code, e.g. 86028807
+       @param amountProtein protein per serving, e.g 15 (g)
+       @param flavor flavor of protein powder, e.g. "chocolate"
+    */
+
+    public ProteinPowder(int serialNumber, int amountProtein, String flavor) {
+	this.flavor = flavor;
+	this.amountProtein = amountProtein;
+	this.serialNumber = serialNumber;
+    }
+
+    /** 
+	Getter for serialNumber
+	@return serial number, numbers under the bar code, e.g. 86028807
+    */
+
+    public int getSerialNumber () { 
+	return this.serialNumber;
+    }
+
+    /** 
+	Getter for amountProtein
+	@return amount of protein per serving, e.g 15 (g)
+    */
+    public int getAmountProtein () {
+	return this.amountProtein; 
+    }
+
+    /**
+	Getter for flavor
+	@return the flavor of the protein powder, e.g. "chocolate"
+    */
+    public String getFlavor () {
+	return this.flavor;
+    }
+
+    /**
+       convert to String representation in format [serialNumber, amountProtein, flavor]
+       e.g. <code>[86028807, 15, "chocolate"]</code>
+
+       @return formatted string, [serialNumber, amountProtein, flavor]
+    */
+
+    @Override
+    public String toString() {
+	return "[" + getSerialNumber() + ", " + getAmountProtein() + ", " + getFlavor() + "]";     
+    }
+
+
+    /** hashcode to be used for hash tables, etc.
+	based only on the serialNumber.
+     */
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        return this.getSerialNumber() % prime;
+    }
+
+    /** compare for equality based on serialNumber
+	@return true if parameter is a ProteinPowder with same serialNumber, otherwise false
+     */
+
+    @Override
+    public boolean equals(Object o) {
+	if (o==null) return false;
+	if (! (o instanceof ProteinPowder) ) return false;
+	ProteinPowder c = (ProteinPowder) o;
+	return (c.getSerialNumber() == this.getSerialNumber());
+    }
+
+}
---
+++ src/edu/ucsb/cs56/w14/lab05/loganortega/ProteinPowderTest.java
@@ -0,0 +1,93 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+
+/** Test class for ProteinPowder
+
+    @author Phill Conrad
+    @author Logan Ortega
+    @author Richard Wang
+    @version 2014/01/30 for lab04, cs56, W14
+    @see ProteinPowder 
+
+*/
+
+public class ProteinPowderTest {
+
+    /** Test case for getSerialNumber method of ProteinPowder
+     @see ProteinPowder#getSerialNumber
+    */
+
+    @Test
+	public void test_getSerialNumber() {
+	ProteinPowder c1 = new ProteinPowder(86028807, 15, "chocolate");
+	assertEquals(86028807,c1.getSerialNumber());
+    }
+
+
+    /** Test case for getAmountProtein method of ProteinPowder
+     @see ProteinPowder#getAmountProtein
+    */
+
+    @Test
+	public void test_getAmountProtein() {
+	ProteinPowder c1 = new ProteinPowder(86028807, 15, "chocolate");
+	assertEquals(15,c1.getAmountProtein());
+    }
+
+    /** Test case for toString
+     @see ProteinPowder#toString
+    */
+
+    @Test
+	public void test_toString() {
+	ProteinPowder c1 = new ProteinPowder(86028807, 15, "chocolate");
+	assertEquals("[86028807, 15, chocolate]",c1.toString());
+    }
+
+    /** Test case 1 for .equals
+     @see ProteinPowder#equals
+    */
+
+    @Test
+	public void test_equals1() {
+	ProteinPowder c1 = new ProteinPowder(86028807, 15, "chocolate");
+	ProteinPowder c2 = new ProteinPowder(86028807, 15, "chocolate");
+	assertTrue(c1.equals(c2));
+    }
+
+    /** Test case 2 for .equals
+     @see ProteinPowder#equals
+    */
+
+    @Test
+	public void test_equals2() {
+	ProteinPowder c1 = new ProteinPowder(86028807, 15, "chocolate");
+	ProteinPowder c2 = new ProteinPowder(99999999, 15, "chocolate");
+	assertFalse(c1.equals(c2));
+    }
+
+    /** Test case 1 for .hashCode
+     @see ProteinPowder#hashCode
+    */
+
+    @Test
+	public void test_hashCode1() {
+	ProteinPowder c1 = new ProteinPowder(86028807, 15, "chocolate");
+	ProteinPowder c2 = new ProteinPowder(86028807, 15, "chocolate");
+	assertTrue(c1.hashCode()==c2.hashCode());
+    }
+
+    /** Test case 2 for .hashCode
+     @see ProteinPowder#hashCode
+    */
+
+    @Test
+	public void test_hashCode2() {
+	ProteinPowder c1 = new ProteinPowder(86028807, 15, "chocolate");
+	ProteinPowder c3 = new ProteinPowder(99999999, 15, "chocolate");
+	assertTrue(c1.hashCode()!=c3.hashCode());
+    }
+
+}
